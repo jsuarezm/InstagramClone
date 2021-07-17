bb12f5985c708cb1b42f4ce8a61214be7c2c6930
Jesus Suarez <jesus.a.suarez@gmail.com>
2021-07-17 22:18:29 +0200

    Added header stack

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -9,19 +10,55 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {StatusBar, Image} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+
 import HomeScreen from './screens/HomeScreen';
 import DiscoveryScreen from './screens/DiscoveryScreen';
 import CreatePostScreen from './screens/CreatePostScreen';
 import NotificationScreen from './screens/NotificationScreen';
 import ProfileScreen from './screens/ProfileScreen';
+
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Foundation from 'react-native-vector-icons/Foundation';
 import Feather from 'react-native-vector-icons/Feather';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
+import logo from './assets/images/logo.png';
+
 const Tab = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Instagram',
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <Feather name="camera" size={25} color={'#545454'} />
+          ),
+          headerLeftContainerStyle: {
+            marginLeft: 15,
+          },
+          headerRight: () => (
+            <Ionicons name="paper-plane-outline" size={25} color={'#545454'} />
+          ),
+          headerRightContainerStyle: {
+            marginRight: 15,
+          },
+          headerTitle: () => (
+            <Image source={logo} resizeMode="contain" style={{width: 130}} />
+          ),
+        }}
+      />
+    </HomeStack.Navigator>
+  );
+}
 
 const App: () => React$Node = () => {
   return (
@@ -46,7 +83,9 @@ const App: () => React$Node = () => {
               return <AntDesign name="hearto" size={size} color={color} />;
             }
             if (route.name === 'Profile') {
-              return <Ionicons name="person-outline" size={size} color={color} />;
+              return (
+                <Ionicons name="person-outline" size={size} color={color} />
+              );
             }
           },
         })}
@@ -55,7 +94,7 @@ const App: () => React$Node = () => {
           inactiveTintColor: 'gray',
           showLabel: false,
         }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={HomeStackScreen} />
         <Tab.Screen name="Discovery" component={DiscoveryScreen} />
         <Tab.Screen name="Post" component={CreatePostScreen} />
         <Tab.Screen name="Notifications" component={NotificationScreen} />
---
+++ src/router/home.routes.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Image} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
+
+import logo from './assets/images/logo.png';
+
+const HomeStack = createStackNavigator();
+
+const HomeRoutes = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Instagram',
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <Feather name="camera" size={25} color={'#545454'} />
+          ),
+          headerLeftContainerStyle: {
+            marginLeft: 15,
+          },
+          headerRight: () => (
+            <Ionicons name="paper-plane-outline" size={25} color={'#545454'} />
+          ),
+          headerRightContainerStyle: {
+            marginRight: 15,
+          },
+          headerTitle: () => (
+            // eslint-disable-next-line react-native/no-inline-styles
+            <Image source={logo} resizeMode="contain" style={{width: 130}} />
+          ),
+        }}
+      />
+    </HomeStack.Navigator>
+  );
+};
+
+export default HomeRoutes;
