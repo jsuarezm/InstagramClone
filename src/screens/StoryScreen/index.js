3dded4d517c10e7760a0415cc44841800ea99640
Jesus Suarez <jesus.a.suarez@gmail.com>
2021-07-26 21:21:19 +0200

    added stories navigation

--- src/screens/StoryScreen/index.js
+++ src/screens/StoryScreen/index.js
@@ -1,45 +1,84 @@
 import React, {useState, useEffect} from 'react';
 import {
   Text,
+  View,
   SafeAreaView,
   ImageBackground,
   ActivityIndicator,
+  TextInput,
+  TouchableWithoutFeedback,
+  Dimensions,
 } from 'react-native';
-import {useRoute} from '@react-navigation/native';
+import {useRoute, useNavigation} from '@react-navigation/native';
+
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import styles from './styles';
 import storiesData from '../../data/stories';
+import ProfilePicture from '../../components/ProfilePicture';
 
 const StoryScreen = () => {
-  const [userStories, setuserStories] = useState(null);
+  const [userStories, setuserStories] = useState(storiesData[0]);
   const [activeStoryIndex, setActiveStoryIndex] = useState(0);
-  const [activeStory, setActiveStory] = useState(null);
+  const [activeStory, setActiveStory] = useState([]);
 
   const route = useRoute();
+  const navigation = useNavigation();
+  const userId = route.params.userId;
+  console.log("user " + userId + "-----" + activeStoryIndex);
+
 
   useEffect(() => {
-    const userId = route.params.userId;
     const userStories = storiesData.find(
       storyData => storyData.user.id === userId,
     );
-    console.log("stories");
     console.log(userStories);
     setuserStories(userStories);
     setActiveStoryIndex(0);
     setActiveStory(userStories.stories[0]);
-    return() => {
-      console.log("fisrt running");
-    };
   }, []);
 
   useEffect(() => {
-    if (userStories && userStories.stories.length > activeStoryIndex - 1) {
-      setActiveStory(userStories.stories[activeStoryIndex]);
-    };
-    return() => {
-      console.log("second running");
-    };
+    if (!userStories) {
+      return;
+    }
+    setActiveStory(userStories.stories[activeStoryIndex])
   }, [activeStoryIndex]);
+
+  const NatigateToNextUser = () => {
+    navigation.push("Story", { userId: (parseInt(userId) + 1).toString() });
+  };
+
+  const NatigateToPrevUser = () => {
+    navigation.push("Story", { userId: (parseInt(userId) - 1).toString() });
+  };
+
+  const handleNextStory = () => {
+    if (activeStoryIndex >= userStories.stories.length - 1) {
+      NatigateToNextUser();
+      return;
+    }
+    setActiveStoryIndex(activeStoryIndex + 1);
+  };
+
+  const handlePrevStory = () => {
+    if (activeStoryIndex <= 0) {
+      NatigateToPrevUser();
+      return;
+    }
+    setActiveStoryIndex(activeStoryIndex - 1);
+  };
+
+  const handlePress = (evt) => {
+    const x = evt.nativeEvent.locationX;
+    const screenWidth = Dimensions.get('window').width;
+    if (x < screenWidth/2) {
+      handlePrevStory();
+    } else {
+      handleNextStory();
+    }
+  };
 
   if (!activeStory) {
     return (
@@ -47,18 +86,39 @@ const StoryScreen = () => {
         <ActivityIndicator />
       </SafeAreaView>
     );
-  };
-  console.log("active story");
-  console.log(activeStory);
-  console.log(activeStory.imageUri);
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <ImageBackground
-        source={{uri: activeStory.imageUri}}
-        resizeMode="cover"
-        style={styles.image}
-      ></ImageBackground>
+      <TouchableWithoutFeedback onPress={handlePress} >
+        <ImageBackground
+          source={{uri: activeStory.imageUri}}
+          style={styles.image}
+        >
+          <View style={styles.userInfo}>
+            <ProfilePicture uri={userStories.user.imageUri} size={50} />
+            <Text style={styles.userName}>{userStories.user.name}</Text>
+            <Text style={styles.postedTime}>{activeStory.postedTime}</Text>
+          </View>
+          <View style={styles.bottomContainer}>
+            <View style={styles.cameraButton}>
+              <Feather name="camera" size={30} color={'#ffffff'} />
+            </View>
+            <View style={styles.textInputContainer}>
+              <TextInput 
+                style={styles.textInput}
+                editable 
+                placeholder="Send Message"
+                placeholderTextColor={"white"}  
+              />
+            </View>
+            <View style={styles.messageButton}>
+              <Ionicons name="paper-plane-outline" size={35} color={'#ffffff'} />
+            </View>
+            
+          </View>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 };
--- src/screens/StoryScreen/styles.js
+++ src/screens/StoryScreen/styles.js
@@ -2,12 +2,60 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    height: "100%",
+    height: '100%',
   },
   image: {
     flex: 1,
     resizeMode: 'cover',
+    justifyContent: 'space-between',
+  },
+  userInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  userName: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  postedTime: {
+    marginLeft: 10,
+    color: "#888888",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  bottomContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  cameraButton: {
+    width: 50,
+    alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 50,
+    marginHorizontal: 10,
+  },
+  messageButton: {
+    width: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    height: "100%",
+    color: 'white',
+    fontSize: 16,
+  },
+  textInputContainer: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'white',
+    marginHorizontal: 10,
+    paddingHorizontal: 10,
+    borderRadius: 50,
+    height: 50,
   },
 });
 
